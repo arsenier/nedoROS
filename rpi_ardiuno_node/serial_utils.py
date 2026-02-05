834dfdf75186f8c0f36e127178efb080f9fdfe63
GlazovYuri <glazov.y0@gmail.com>
2026-02-05 18:16:35 +0300

    new message interface

--- rpi_ardiuno_node/serial_utils.py
+++ rpi_ardiuno_node/serial_utils.py
@@ -9,8 +9,15 @@ port = "COM3"
 baudrate = 9600
 
 CONTROL_HEADER = 0x01
-TX_PACKET_SIZE = 1 + 4 + 4 + 1  # 10 bytes
-RX_PACKET_SIZE = 4 + 4 + 4 + 1 + 1  # 14 bytes
+TX_PACKET_SIZE = 1 + 4 + 4 + 1 + 1  # 14 bytes
+RX_PACKET_SIZE = 1 + 4 + 4 + 4 + 1 + 1 + 1  # 16 bytes
+
+
+def xor_checksum(data: bytes) -> int:
+    checksum = 0
+    for b in data:
+        checksum ^= b
+    return checksum
 
 
 def send_speeds(
@@ -19,13 +26,16 @@ def send_speeds(
     ser: Optional[serial.Serial] = None
     ans: Optional[tuple[float, float, float, bool, bool]] = None
 
-    command = struct.pack(
-        "<BffB",
+    args: list[float] = [
         CONTROL_HEADER,
         left_motor_speed,
         right_motor_speed,
         int(gripper),
-    )
+    ]
+
+    checksum = xor_checksum(struct.pack("<BffB", *args))
+    args.append(checksum)
+    command = struct.pack("<BffBB", *args)
 
     try:
         ser = serial.Serial(port, baudrate=baudrate, timeout=1)
@@ -36,6 +46,14 @@ def send_speeds(
         response = ser.read(RX_PACKET_SIZE)
         if len(response) != RX_PACKET_SIZE:
             print("Invalid response size:", len(response))
+            return None
+
+        checksum = 0
+        for byte in response:
+            checksum ^= byte
+
+        if checksum != 0:
+            print("Invalid checksum:", checksum)
             return None
 
         ans = struct.unpack("<fffBB", response)
@@ -61,12 +79,14 @@ if __name__ == "__main__":
     send_speeds(5, 0.5, False)
 
     # data_to_write = struct.pack(
-    #     "<fffBB",
+    #     "<BfffBBB",
+    #     0x01,
     #     0.5,
     #     4.125,
     #     4.25,
     #     int(True),
     #     int(False),
+    #     0x32,
     # )
 
     # with open("output.bin", "wb") as binary_file:
@@ -76,11 +96,14 @@ if __name__ == "__main__":
     #     response = binary_file.read(RX_PACKET_SIZE)
     #     hexdump.hexdump(response)
 
-    #     ans = struct.unpack("<fffBB", response)
+    #     if xor_checksum(response) != 0:
+    #         print("Invalid checksum")
+
+    #     ans = struct.unpack("<BfffBBB", response)
 
     #     print("Received:")
-    #     print("  x =", ans[0])
-    #     print("  y =", ans[1])
-    #     print("  theta =", ans[2])
-    #     print("  left_usik =", bool(ans[3]))
-    #     print("  right_usik =", bool(ans[4]))
+    #     print("  x =", ans[1])
+    #     print("  y =", ans[2])
+    #     print("  theta =", ans[3])
+    #     print("  left_usik =", bool(ans[4]))
+    #     print("  right_usik =", bool(ans[5]))
