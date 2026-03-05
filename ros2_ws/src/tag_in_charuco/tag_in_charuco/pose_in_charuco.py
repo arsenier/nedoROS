ca0a90c680bfd0aa657a07c2682e386ec7546b63
GlazovYuri <glazov.y0@gmail.com>
2026-03-05 18:53:28 +0300

    add /map topic with odometry

--- ros2_ws/src/tag_in_charuco/tag_in_charuco/pose_in_charuco.py
+++ ros2_ws/src/tag_in_charuco/tag_in_charuco/pose_in_charuco.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-import time
 import math
 import numpy as np
 
 import rclpy
 from rclpy.node import Node
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+import tf2_geometry_msgs
 
 def quat_to_rot(q):
     x, y, z, w = q.x, q.y, q.z, q.w
@@ -24,11 +27,15 @@ def quat_to_rot(q):
 class XYYawInCharuco(Node):
     def __init__(self):
         super().__init__('tag_xyyaw_in_charuco')
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
 
         self.tag_topic = self.declare_parameter('tag_topic', '/image_raw_apriltag_pose').value
         self.charuco_topic = self.declare_parameter('charuco_topic', '/image_raw_charuco_pose').value
         self.slop = float(self.declare_parameter('sync_slop_sec', 0.02).value)
         self.print_hz = float(self.declare_parameter('print_hz', 5.0).value)
+
+        self.odom_publisher_ = self.create_publisher(Odometry, '/map', 10)
 
         self._last_print = 0.0
 
@@ -42,30 +49,32 @@ class XYYawInCharuco(Node):
         self.get_logger().info(f"Listening tag={self.tag_topic} charuco={self.charuco_topic} slop={self.slop}s")
 
     def cb(self, tag_msg: PoseStamped, ch_msg: PoseStamped):
-        now = time.time()
-        if self.print_hz > 0 and (now - self._last_print) < (1.0 / self.print_hz):
-            return
-        self._last_print = now
+        now = rclpy.time.Time()
 
-        if tag_msg.header.frame_id != ch_msg.header.frame_id:
-            self.get_logger().warn(f"frame mismatch: tag='{tag_msg.header.frame_id}' charuco='{ch_msg.header.frame_id}'")
-            return
 
-        R_ct = quat_to_rot(tag_msg.pose.orientation)     # R_cam_tag
-        R_cc = quat_to_rot(ch_msg.pose.orientation)      # R_cam_charuco
+        trans = self.tf_buffer.lookup_transform(
+            'calib_charuco',
+            'default_cam',
+            now)
+        
+        point_on_board = tf2_geometry_msgs.do_transform_pose_stamped(tag_msg, trans)
 
-        t_ct = np.array([tag_msg.pose.position.x, tag_msg.pose.position.y, tag_msg.pose.position.z], dtype=np.float64)
-        t_cc = np.array([ch_msg.pose.position.x, ch_msg.pose.position.y, ch_msg.pose.position.z], dtype=np.float64)
+        angle = math.atan2(point_on_board.pose.orientation.w, point_on_board.pose.orientation.z)
+        self.get_logger().info(f"x={point_on_board.pose.position.x*10:+.4f} m  y={point_on_board.pose.position.y*10:+.4f} m yaw={angle:+.2f} rad")
 
-        # tag position in charuco
-        t_ch_tag = R_cc.T @ (t_ct - t_cc)
+        self.publish_odometry(point_on_board)
 
-        # relative rotation charuco->tag
-        R_ch_tag = R_cc.T @ R_ct
-        yaw = math.atan2(R_ch_tag[1, 0], R_ch_tag[0, 0])   # around Z of charuco
-        yaw_deg = yaw * 180.0 / math.pi
-
-        self.get_logger().info(f"x={t_ch_tag[0]*10:+.4f} m  y={t_ch_tag[1]*10:+.4f} m yaw={yaw_deg:+.2f} deg")
+    def publish_odometry(self, pose_stamped: PoseStamped):
+        msg = Odometry()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.pose.pose.position.x = pose_stamped.pose.position.x*10
+        msg.pose.pose.position.y = pose_stamped.pose.position.y*10
+        msg.pose.pose.position.z = 0.0
+        msg.pose.pose.orientation.x =0.0
+        msg.pose.pose.orientation.y =0.0
+        msg.pose.pose.orientation.z = pose_stamped.pose.orientation.z
+        msg.pose.pose.orientation.w = pose_stamped.pose.orientation.w
+        self.odom_publisher_.publish(msg)
 
 
 def main():
