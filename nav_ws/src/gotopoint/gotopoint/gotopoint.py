097f43d56c813a03fd6201d45de41265f5c26e9a
sudo14121 <9112554233n@gmail.com>
2026-03-12 18:58:31 +0300

    lidar tf and si navigation

--- nav_ws/src/gotopoint/gotopoint/gotopoint.py
+++ nav_ws/src/gotopoint/gotopoint/gotopoint.py
@@ -48,12 +48,12 @@ class TurtlePoseFollower(Node):
         self.y_goal = 0.0
         self.angle_pos = 0.0
 
-        self.k_forward = 0.52
-        self.k_turn = 0.4
+        self.k_forward = 0.58
+        self.k_turn = 1.5
         self.maxline = 0.2
-        self.maxz = 1.0
+        self.maxz = 0.8
 
-        self.distotcl = 0.05
+        self.distotcl = 0.1
 
         self.flag_going = False
         self.turning = True
@@ -78,55 +78,93 @@ class TurtlePoseFollower(Node):
         self.current_pose.x = msg.pose.position.x
         self.current_pose.y = msg.pose.position.y
         self.current_pose.theta = math.atan2(msg.pose.orientation.z, msg.pose.orientation.w) * 2
-        self.get_logger().info(f'Нам камера пишет) {self.current_pose}')
+        # self.get_logger().info(f'Нам камера пишет) {self.current_pose}')
 
     def pose_callback_robot(self, msg):
         # self.current_pose = msg.pose.pose
-        self.current_pose = Pose()
-        self.current_pose.x = msg.pose.pose.position.x
-        self.current_pose.y = msg.pose.pose.position.y
-        self.current_pose.theta = math.atan2(msg.pose.pose.orientation.z, msg.pose.pose.orientation.w) * 2
-        self.get_logger().info(f'Нам написали) {self.current_pose}')
+        robot_pose = Pose()
+        robot_pose.x = msg.pose.pose.position.x
+        robot_pose.y = msg.pose.pose.position.y
+        robot_pose.theta = math.atan2(msg.pose.pose.orientation.z, msg.pose.pose.orientation.w) * 2
+        self.get_logger().info(f'Нам написали) {robot_pose}')
 
     def control_loop(self):
+        # if distance < self.distotcl and self.flag_going:
+        #     twist = Twist()
+        #     twist.linear.x = 0.0
+        #     twist.angular.z = 0.0
+        #     self.publisher_.publish(twist)
+        #     self.amgoing = True
+        #     return 
+        # elif distance < self.distotcl:
+        #     self.turning = False
+        #     desired_angle = self.angle_pos
+        #     angle_error = desired_angle - self.current_pose.theta
+        #     angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
+        #     self.get_logger().info(f'ошибка поворота) {angle_error}')
+        #     if abs(angle_error) < self.distotcl:
+        #         self.flag_going = True
+        # else:
+        #     desired_angle = math.atan2(dy, dx)
+        #     angle_error = desired_angle - self.current_pose.theta
+        #     angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
+        # ####
+        # #####
+
+        # twist = Twist()
+        # if (angle_error < 0.2 and angle_error > -0.2 and self.turning):
+        #     twist.linear.x = self.k_forward * distance
+        #     if twist.linear.x > self.maxline:
+        #         twist.linear.x = self.maxline
+        # else:
+        #     twist.linear.x = 0.0
+
+        # twist.angular.z = self.k_turn * angle_error
+        # if twist.angular.z > self.maxz:
+        #     twist.angular.z = self.maxz
+        # elif twist.angular.z < -self.maxz:
+        #     twist.angular.z = -self.maxz
+
+        anglereg = 0.0
+        v = 0.0
+
         dx = self.x_goal - self.current_pose.x
         dy = self.y_goal - self.current_pose.y
         distance = math.sqrt(dx*dx + dy*dy)
+        desired_angle = math.atan2(dy, dx)
+        angle_errorToFr = desired_angle - self.current_pose.theta
+        angle_errorToFr = math.atan2(math.sin(angle_errorToFr), math.cos(angle_errorToFr))
 
-        if distance < self.distotcl and self.flag_going:
-            twist = Twist()
-            twist.linear.x = 0.0
-            twist.angular.z = 0.0
-            self.publisher_.publish(twist)
-            self.amgoing = True
-            return 
-        elif distance < self.distotcl:
-            self.turning = False
-            desired_angle = self.angle_pos
-            angle_error = desired_angle - self.current_pose.theta
-            angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
-            if abs(angle_error) < self.distotcl:
-                self.flag_going = True
+        desired_angle = self.angle_pos
+        angle_error = desired_angle - self.current_pose.theta
+        angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
+
+        if abs(angle_errorToFr) > 0.2 and abs(distance) > self.distotcl:
+            amglereg = angle_errorToFr
+        elif abs(distance) > self.distotcl:
+            v = distance
+            amglereg = angle_errorToFr
+        elif abs(angle_error) > 0.1:
+            amglereg = angle_error
         else:
-            desired_angle = math.atan2(dy, dx)
-            angle_error = desired_angle - self.current_pose.theta
-            angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
-        ####
-        #####
+            v = 0
+            amglereg = 0
 
         twist = Twist()
-        if (angle_error < 1 and angle_error > -1 and self.turning):
-            twist.linear.x = self.k_forward * distance
-            if twist.linear.x > self.maxline:
-                twist.linear.x = self.maxline
-        else:
-            twist.linear.x = 0.0
 
-        twist.angular.z = self.k_turn * angle_error
+        twist.linear.x = self.k_forward * v
+        if twist.linear.x > self.maxline:
+            twist.linear.x = self.maxline
+
+        twist.angular.z = self.k_turn * amglereg
         if twist.angular.z > self.maxz:
             twist.angular.z = self.maxz
         elif twist.angular.z < -self.maxz:
             twist.angular.z = -self.maxz
+
+        # self.get_logger().info(f'eps: {angle_errorToFr}, dtheta: {angle_error}, S: {distance}')
+        # self.get_logger().info(f'ошибка поворота) {amglereg}')
+        # self.get_logger().info(f'расстояние вперед) {distance}')
 
         self.publisher_.publish(twist)
         
---
+++ nav_ws/src/tf_broadcaster/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'tf_broadcaster'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='katya',
+    maintainer_email='9112554233n@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'tf_broadcaster = tf_broadcaster.tf_broadcaster:main'
+        ],
+    },
+)
